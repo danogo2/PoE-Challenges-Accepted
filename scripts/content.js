93bfe565d2e06d0173dfdbe01dd5ad987dbfc00d
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2024-03-05 15:27:06 +0100

    add dropdown menu and json folder

--- scripts/content.js
+++ scripts/content.js
@@ -31,6 +31,12 @@
   const svgIconExport =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#beb698" d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V288H216c-13.3 0-24 10.7-24 24s10.7 24 24 24H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM384 336V288H494.1l-39-39c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l80 80c9.4 9.4 9.4 24.6 0 33.9l-80 80c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l39-39H384zm0-208H256V0L384 128z"/></svg>';
 
+  const svgIconTags =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#beb698" d="M345 39.1L472.8 168.4c52.4 53 52.4 138.2 0 191.2L360.8 472.9c-9.3 9.4-24.5 9.5-33.9 .2s-9.5-24.5-.2-33.9L438.6 325.9c33.9-34.3 33.9-89.4 0-123.7L310.9 72.9c-9.3-9.4-9.2-24.6 .2-33.9s24.6-9.2 33.9 .2zM0 229.5V80C0 53.5 21.5 32 48 32H197.5c17 0 33.3 6.7 45.3 18.7l168 168c25 25 25 65.5 0 90.5L277.3 442.7c-25 25-65.5 25-90.5 0l-168-168C6.7 262.7 0 246.5 0 229.5zM144 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>';
+
+  const svgIconMenu =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#beb698" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>';
+
   // decodeURIComponent is necessary for leagues separated by /
   const state = {
     challElMap: new Map(),
@@ -45,79 +51,7 @@
     ),
     hideCompleted: false,
     lastSelectedTagValue: '',
-    defaultTags: [
-      ['vendor', 'buy', 'recipe'],
-      ['lab', 'labyrinth', 'divine font', 'transfigured', 'treasure key'],
-      [
-        'boss',
-        'atziri, queen',
-        'cortex',
-        'the elder',
-        'the shaper',
-        'the maven',
-        'catarina',
-        'infinite hunger',
-        'eater of worlds',
-        'trialmaster',
-        'oshabi',
-        'omnitect',
-        'lycia, unholy',
-      ],
-      ['affliction', 'wildwood', 'wisp', 'tincture', 'charm'],
-      ['shrine'],
-      ['essence'],
-      ['einhar', 'beasts', 'bestiary'],
-      ['delirium', 'simulacrum'],
-      ['torment', 'touched', 'possessed'],
-      [
-        'leveling',
-        'act 1',
-        'act 2',
-        'act 3',
-        'act 4',
-        'act 5',
-        'act 6',
-        'act 7',
-        'act 8',
-        'act 9',
-      ],
-      ['anarchy', 'rogue exile'],
-      ['ambush', 'strongbox'],
-      ['map', 'tier', 'quantity', 'rarity'],
-      ['beyond', 'tainted'],
-      ['blight', 'cassia', 'ravaged', ' oil'],
-      ['expedition', 'remnant', 'logbook', 'artifact'],
-      ['ultimatum', 'inscribed', 'trialmaster', 'catalyst'],
-      ['alva', 'incursion', 'omnitect'],
-      ['jun', 'betrayal', 'catarina', 'syndicate'],
-      ['niko', 'delve', 'resonator', 'fossil'],
-      ['divination', 'turn in'],
-      ['scarab'],
-      ['breach'],
-      ['tempest'],
-      ['harvest', 'lifeforce', 'oshabi'],
-      ['kirac'],
-      ['harbinger', 'fracturing', 'beachhead'],
-      ['vaal'],
-      ['monster'],
-      ['memory'],
-      ['essence'],
-      ['heist', 'tempering', 'tailoring', 'blueprint', 'contract'],
-      ['eldritch'],
-      ['ritual', 'altar', 'vessel'],
-      ['legion', 'timeless'],
-      [
-        'sanctum',
-        'relic',
-        'divinia',
-        'lycia, unholy',
-        'xenathar',
-        'braom',
-        'varakath',
-      ],
-      ['pinnacle'],
-      ['atlas'],
-    ],
+    defaultTags: [],
     updatedDefaultTags: new Set(),
     pinOrdersSet: new Set([0]),
     hideNotes: false,
@@ -241,7 +175,7 @@
   const insertHideButtonEl = parentEl => {
     parentEl.insertAdjacentHTML(
       'beforeend',
-      `<div class="settings-option"><button class='button-settings button-hide'><div class="settings-icon icon-hide" title="hide completed">${svgIconEye}</div><div class="settings-icon icon-show" title="show completed">${svgIconEyeOff}</div></button></div>`
+      `<div class="settings-option"><button class='button-settings button-hide'><div class="settings-icon icon-hide" title="Hide completed challenges">${svgIconEye}</div><div class="settings-icon icon-show" title="show completed">${svgIconEyeOff}</div></button></div>`
     );
     return parentEl.querySelector('.button-hide');
   };
@@ -249,7 +183,7 @@
   const insertClearButtonEl = parentEl => {
     parentEl.insertAdjacentHTML(
       'beforeend',
-      `<div class="settings-option"><button class='button-settings button-clear'><div class="settings-icon icon-clear" title="clear league">${svgIconTrash}</div></button></div>`
+      `<div class="menu-option"><button class='button-settings button-clear'><div class="settings-icon icon-clear">${svgIconTrash}</div><div class="menu-text">Clear league</div></button></div>`
     );
     return parentEl.querySelector('.button-clear');
   };
@@ -257,7 +191,7 @@
   const insertExportButtonEl = parentEl => {
     parentEl.insertAdjacentHTML(
       'beforeend',
-      `<div class="settings-option"><button class='button-settings button-export'><div class="settings-icon icon-export" title="export tags and notes">${svgIconExport}</div></button></div>`
+      `<div class="menu-option"><button class='button-settings button-export'><div class="settings-icon icon-export">${svgIconExport}</div><div class="menu-text">Export tags and notes</div></button></div>`
     );
     return parentEl.querySelector('.button-export');
   };
@@ -265,7 +199,7 @@
   const insertImportButtonEl = parentEl => {
     parentEl.insertAdjacentHTML(
       'beforeend',
-      `<div class="settings-option"><button class='button-settings button-import'><div class="settings-icon icon-import" title="import tags and notes">${svgIconImport}</div></button></div>`
+      `<div class="menu-option"><button class='button-settings button-import'><div class="settings-icon icon-import">${svgIconImport}</div><div class="menu-text">Import tags and notes</div></button></div>`
     );
     return parentEl.querySelector('.button-import');
   };
@@ -284,6 +218,14 @@
       '<div class="settings-option"><select class="tag-select" name="tags" id="tags"><option class="tag-option" value="">— Tags —</option></select></div>'
     );
     return parentEl.querySelector('#tags');
+  };
+
+  const insertTagsEditButtonEl = parentEl => {
+    parentEl.insertAdjacentHTML(
+      'beforeend',
+      `<div class="menu-option"><button class='button-settings button-tags-settings'><div class="settings-icon icon-tags">${svgIconTags}</div><div class="menu-text">Edit default tags</div></button></div>`
+    );
+    return parentEl.querySelector('.button-tags-settings');
   };
 
   const insertTagInputEl = (parentEl, id) => {
@@ -308,6 +250,22 @@
       `<div class="note-container"><textarea name="textarea" class="note-textarea" placeholder="Write your note here" data-id="${id}" wrap="hard"></textarea>`
     );
     return parentEl.querySelector('.note-textarea');
+  };
+
+  const insertMenuButtonEl = parentEl => {
+    parentEl.insertAdjacentHTML(
+      'beforeend',
+      `<div class="settings-option"><button class='button-settings button-menu'><div class="settings-icon icon-menu" title="Customize and control challenges">${svgIconMenu}</div></button></div>`
+    );
+    return parentEl.querySelector('.button-menu');
+  };
+
+  const insertMenuDialogEl = parentEl => {
+    parentEl.insertAdjacentHTML(
+      'beforeend',
+      '<dialog class="menu-dialog"></dialog>'
+    );
+    return parentEl.querySelector('.menu-dialog');
   };
 
   // using this method because {element.innerHTML = ''}doesn't clear event handlers of the child nodes and might cause memory leak
@@ -595,8 +553,19 @@
     }
   };
 
-  const addEventHandler = (element, eventType, handler) => {
-    element.addEventListener(eventType, event => handler(event));
+  const clickMenuButtonHandler = (event, menuDialogEl) => {
+    event.stopPropagation();
+    if (menuDialogEl.open) {
+      menuDialogEl.close();
+    } else {
+      menuDialogEl.show();
+    }
+  };
+
+  const addEventHandler = (element, eventType, handler, ...handlerArgs) => {
+    element.addEventListener(eventType, event =>
+      handler(event, ...handlerArgs)
+    );
   };
 
   const changeTopLayout = () => {
@@ -611,14 +580,16 @@
     infoEl.textContent = '';
     infoEl.insertAdjacentHTML('beforeend', '<div class="settings"></div>');
     const settingsEl = infoEl.querySelector('.settings');
-
     const searchInputEl = insertSearchInputEl(settingsEl);
     const tagSelectEl = insertTagSelectEl(settingsEl);
     settingsEl.insertAdjacentElement('beforeend', leagueSelectEl);
     const hideButtonEl = insertHideButtonEl(settingsEl);
-    const clearButtonEl = insertClearButtonEl(settingsEl);
-    const importButtonEl = insertImportButtonEl(settingsEl);
-    const exportButtonEl = insertExportButtonEl(settingsEl);
+    const menuButtonEl = insertMenuButtonEl(settingsEl);
+    const menuDialogEl = insertMenuDialogEl(settingsEl);
+    const tagsEditButtonEl = insertTagsEditButtonEl(menuDialogEl);
+    const importButtonEl = insertImportButtonEl(menuDialogEl);
+    const exportButtonEl = insertExportButtonEl(menuDialogEl);
+    const clearButtonEl = insertClearButtonEl(menuDialogEl);
     titleEl.textContent = titleEl.textContent.replace('Challenges', '').trim();
     settingsEl.insertAdjacentElement('beforeend', titleEl);
 
@@ -650,6 +621,23 @@
         }
       });
       reader.readAsText(file);
+    });
+    addEventHandler(
+      menuButtonEl,
+      'click',
+      clickMenuButtonHandler,
+      menuDialogEl
+    );
+    window.addEventListener('click', e => {
+      const dialogDimensions = menuDialogEl.getBoundingClientRect();
+      if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+      ) {
+        menuDialogEl.close();
+      }
     });
   };
 
